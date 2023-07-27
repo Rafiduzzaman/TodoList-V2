916a1cf650ebfd4afc24718a6bd6db0b0ceee68a
codeStackr <rdzaman20000@gmail.com>
2023-07-27 17:34:45 +0600

    fixed linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import _ from "lodash";
-import "./index.css";
+import { sortBy } from 'lodash'; // Import the specific function from lodash
+
+import './index.css';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
-  element.classList.add("hello");
+  element.classList.add('hello');
 
   return element;
 }
@@ -12,43 +13,45 @@ function component() {
 function Render() {
   const tasks = [
     {
-      description: "Buy groceries",
+      description: 'Buy groceries',
       completed: false,
       index: 1,
     },
     {
-      description: "Clean the house",
+      description: 'Clean the house',
       completed: true,
       index: 2,
     },
     {
-      description: "Finish homework",
+      description: 'Finish homework',
       completed: false,
       index: 3,
     },
     {
-      description: "Go for a walk",
+      description: 'Go for a walk',
       completed: false,
       index: 4,
     },
   ];
-  function sortTasksByIndex() {
-    tasks.sort((a, b) => a.index - b.index);
-  }
-  function populateTodoList() {
-    const todoList = document.getElementById("todoList");
 
-    tasks.forEach((task) => {
-      const listItem = document.createElement("li");
+  // Use _.sortBy from lodash to sort tasks by index
+  const sortedTasks = sortBy(tasks, 'index');
+
+  function populateTodoList() {
+    const todoList = document.getElementById('todoList');
+
+    sortedTasks.forEach((task) => {
+      const listItem = document.createElement('li');
       listItem.textContent = task.description;
       if (task.completed) {
-        listItem.style.textDecoration = "line-through";
+        listItem.style.textDecoration = 'line-through';
       }
       todoList.appendChild(listItem);
     });
   }
-  sortTasksByIndex();
+
   populateTodoList();
 }
+
 Render();
 document.body.appendChild(component());
