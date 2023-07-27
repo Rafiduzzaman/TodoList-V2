762c4c14a4c67bbd6ecc1f40e9da39dbdb4e2f4c
codeStackr <rdzaman20000@gmail.com>
2023-07-27 17:28:05 +0600

    Added to do list structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
-import _ from 'lodash';
-import './index.css';
+import _ from "lodash";
+import "./index.css";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.classList.add("hello");
 
   return element;
 }
 
+function Render() {
+  const tasks = [
+    {
+      description: "Buy groceries",
+      completed: false,
+      index: 1,
+    },
+    {
+      description: "Clean the house",
+      completed: true,
+      index: 2,
+    },
+    {
+      description: "Finish homework",
+      completed: false,
+      index: 3,
+    },
+    {
+      description: "Go for a walk",
+      completed: false,
+      index: 4,
+    },
+  ];
+  function sortTasksByIndex() {
+    tasks.sort((a, b) => a.index - b.index);
+  }
+  function populateTodoList() {
+    const todoList = document.getElementById("todoList");
+
+    tasks.forEach((task) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = task.description;
+      if (task.completed) {
+        listItem.style.textDecoration = "line-through";
+      }
+      todoList.appendChild(listItem);
+    });
+  }
+  sortTasksByIndex();
+  populateTodoList();
+}
+Render();
 document.body.appendChild(component());
