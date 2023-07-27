cf6d5bc85eece6ede84a41687cf6da29c2ad0aaa
Md.Rafiduzzaman Nahin <103636372+Rdzaman20000@users.noreply.github.com>
2023-07-27 19:24:12 +0600

    Added to do list structure (#2)
    * Added to do list structure
    
    * fixed linters errors
    
    * file change during build
    
    * Edited live demo part

--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
-import _ from 'lodash';
+import { sortBy } from 'lodash'; // Import the specific function from lodash
+
 import './index.css';
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
   return element;
 }
 
+function Render() {
+  const tasks = [
+    {
+      description: 'Buy groceries',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Clean the house',
+      completed: true,
+      index: 2,
+    },
+    {
+      description: 'Finish homework',
+      completed: false,
+      index: 3,
+    },
+    {
+      description: 'Go for a walk',
+      completed: false,
+      index: 4,
+    },
+  ];
+
+  // Use _.sortBy from lodash to sort tasks by index
+  const sortedTasks = sortBy(tasks, 'index');
+
+  function populateTodoList() {
+    const todoList = document.getElementById('todoList');
+
+    sortedTasks.forEach((task) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = task.description;
+      if (task.completed) {
+        listItem.style.textDecoration = 'line-through';
+      }
+      todoList.appendChild(listItem);
+    });
+  }
+
+  populateTodoList();
+}
+
+Render();
 document.body.appendChild(component());
