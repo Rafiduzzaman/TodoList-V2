379eb9b799f342f18cf886eb92c4bc11539ee991
codeStackr <rdzaman20000@gmail.com>
2023-07-29 11:46:12 +0600

    Added clearCompleted task

--- src/Status.js
+++ src/Status.js
@@ -2,7 +2,6 @@ function updateTaskStatus(taskIndex, status, tasksLocal) {
   tasksLocal[taskIndex - 1].completed = status;
   localStorage.setItem('tasks', JSON.stringify(tasksLocal));
 }
-
 function clearCompletedTasks(tasksLocal) {
   const updatedTasks = tasksLocal.filter((task) => !task.completed)
     .map((task, index) => ({
@@ -10,7 +9,7 @@ function clearCompletedTasks(tasksLocal) {
       index: index + 1,
     }));
 
-  localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
   document.location.reload();
 }
 
